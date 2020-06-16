085fe427d28e292ede4a83562ee1d9a9febdd836
JustAHackers <47746718+JustAHackers@users.noreply.github.com>
2020-06-16 22:22:42 +0000

    Add files via upload

--- sele.py
+++ sele.py
@@ -4,13 +4,16 @@ import requests
 from multiprocessing.pool import ThreadPool
 pl = []
 prolist = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all").text
+prolist1 = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all").text
+prolis2 = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all").text
 my_url = "https://www.youtube.com/watch?v=Az-SIHZhzgc&list=PLZzMHROnvDL_qhylK7TOLgbRSc7ROwwAO&index=1"
+
 def tri(i):
    try:
     if requests.get(my_url,proxies={"https":"https://"+i},timeout=1).status_code == 200:
-       tro(i)
        print (i)
-       time.sleep(1)
+       pl.append(i)
+       tro(i)
    except:
     pass
 
@@ -20,8 +23,9 @@ def tro(i):
             driver = webdriver.Chrome("chromedriver.exe",chrome_options=chrome_options)
             driver.get("https://youtube.com/c/rezondegrowtopia")
             driver.get(my_url)
-            time.sleep(7)
             driver.minimize_window()
 
-tp = ThreadPool(1000)
+tp = ThreadPool(1500)
 tp.map(tri,prolist.splitlines())
+tp.map(tri,prolist1.splitlines())
+tp.map(tri,prolist2.splitlines())
