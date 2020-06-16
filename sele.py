379b6bc10f5b856a5b2db0d017b73e6241917022
JustAHackers <47746718+JustAHackers@users.noreply.github.com>
2020-06-16 08:50:28 +0000

    Add files via upload

--- sele.py
+++ sele.py
@@ -2,18 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 from multiprocessing.pool import ThreadPool
+pl = []
 prolist = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all").text
-my_url = "https://www.youtube.com/playlist?list=PLZzMHROnvDL_qhylK7TOLgbRSc7ROwwAO"
+my_url = "https://www.youtube.com/watch?v=Az-SIHZhzgc&list=PLZzMHROnvDL_qhylK7TOLgbRSc7ROwwAO&index=1"
+def tri(i):
+   try:
+    if requests.get(my_url,proxies={"https":"https://"+i},timeout=1).status_code == 200:
+       tro(i)
+       print (i)
+       time.sleep(1)
+   except:
+    pass
+
 def tro(i):
-    try:
-     if requests.get(my_url,proxies={"https":"https://"+i}).status_code == 200:
             chrome_options = Options()
             chrome_options.add_argument("--proxy-server="+i)    
-            driver = webdriver.Chrome(chrome_options=chrome_options)
+            driver = webdriver.Chrome("chromedriver.exe",chrome_options=chrome_options)
+            driver.get("https://youtube.com/c/rezondegrowtopia")
             driver.get(my_url)
-            tombol = driver.find_elements_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-browse[3]/ytd-playlist-sidebar-renderer/div/ytd-playlist-sidebar-primary-info-renderer/div[4]/ytd-menu-renderer/div/ytd-button-renderer[1]/a/yt-icon-button/button")
-            tombol[0].click()
-    except:
-       pass
-tp = ThreadPool(100)
-tp.map(tro,prolist.splitlines())
+            driver.minimize_window()
+
+tp = ThreadPool(1000)
+tp.map(tri,prolist.splitlines())
