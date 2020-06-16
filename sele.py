f423d98c71e93af96d6cb8e94219d3d0d015bfcf
JustAHackers <47746718+JustAHackers@users.noreply.github.com>
2020-06-16 22:24:21 +0000

    Add files via upload

--- sele.py
+++ sele.py
@@ -5,7 +5,7 @@ from multiprocessing.pool import ThreadPool
 pl = []
 prolist = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all").text
 prolist1 = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all").text
-prolis2 = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all").text
+prolis2 = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=10000&country=all").text
 my_url = "https://www.youtube.com/watch?v=Az-SIHZhzgc&list=PLZzMHROnvDL_qhylK7TOLgbRSc7ROwwAO&index=1"
 
 def tri(i):
