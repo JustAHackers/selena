30e79e4217ad9da4394a3d47402c998c458aac5b
JustAHackers <47746718+JustAHackers@users.noreply.github.com>
2020-06-18 06:30:25 +0000

    Add files via upload

--- sele.py
+++ sele.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import requests,time
+import requests
 from multiprocessing.pool import ThreadPool
+import time
 pl = []
 prolist = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all").text
 prolist1 = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all").text
 prolist2 = requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=10000&country=all").text
+dl = []
 my_url = "https://www.youtube.com/watch?v=Az-SIHZhzgc&list=PLZzMHROnvDL_qhylK7TOLgbRSc7ROwwAO&index=1"
 
 def tri(i):
    try:
-    if requests.get(my_url,proxies={"https":"https://"+i},timeout=0.3).status_code == 200:
+    if requests.get(my_url,proxies={"https":"https://"+i},timeout=1).status_code == 200:
        print (i)
        pl.append(i)
        tro(i)
@@ -23,8 +25,12 @@ def tro(i):
             driver = webdriver.Chrome("chromedriver.exe",chrome_options=chrome_options)
             driver.get("https://youtube.com/c/rezondegrowtopia")
             driver.get(my_url)
-            driver.minimize_window()
             time.sleep(7)
+            dl.append(driver)
 
-tp = ThreadPool(1500)
+tp = ThreadPool(5000)
 tp.map(tri,prolist.splitlines())
+tp.map(tri,prolist1.splitlines())
+tp.map(tri,prolist2.splitlines())
+for i in dl:
+    i.minimize_window()
